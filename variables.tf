c433171e6c078d85e7b73e540cf439fdce2d2e15
Alex Hunt <alex.hunt@materialize.com>
2025-03-25 10:26:53 +0000

    Bump helm chart to v25.1.4

--- variables.tf
+++ variables.tf
@@ -11,7 +11,8 @@ variable "environment" {
 variable "operator_version" {
   description = "Version of the Materialize operator to install"
   type        = string
-  default     = "v25.1.2"
+  default     = "v25.1.4"
+  nullable    = false
 }
 
 variable "helm_repository" {
