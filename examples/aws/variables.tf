ed00a44f75b5661fa5f7ac4c63e0430c5f056753
kay-kim <kay.kim@materialize.com>
2025-03-26 09:38:59 -0400

    Revert "rm defaults from examples"
    This reverts commit 9716882ad2791c63d62dd53056158edf4d68142a.

--- examples/aws/variables.tf
+++ examples/aws/variables.tf
@@ -41,6 +41,7 @@ variable "helm_values" {
 variable "orchestratord_version" {
   description = "Version of the Materialize orchestrator to install"
   type        = string
+  default     = "v0.138.0"
 }
 
 variable "operator_namespace" {
